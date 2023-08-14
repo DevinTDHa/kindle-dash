37bf691e2037e232700496faf6b29037fa77b7c4
Devin Ha <t.ha@tu-berlin.de>
2023-08-14 22:26:40 +0200

    Stop now restores initial state

--- src/stop.sh
+++ src/stop.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env sh
 pkill -f dash.sh
+lipc-set-prop com.lab126.pillow disableEnablePillow enable
+sleep 1
+initctl start framework
+sleep 1
+initctl start webreader
+sleep 1
+echo ondemand >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+lipc-set-prop com.lab126.powerd preventScreenSaver 0
